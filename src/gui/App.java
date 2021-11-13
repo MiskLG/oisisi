a03e10c7b89a40cad4730599da1da8b1d94c9477
Dusko Radicic <radicic.ra129.2019@uns.ac.rs>
2021-11-13 18:23:39 +0100

    main window running

--- src/gui/App.java
+++ src/gui/App.java
@@ -1,9 +1,13 @@
 package gui;
 
+import javax.swing.WindowConstants;
+
 public class App {
 
 	public static void main(String[] args) {
-		
+		MainWindow mw = new MainWindow();
+		mw.setVisible(true);
+		mw.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 
 }
