19e03683d0b9dd4a4f4ad7b3d0ed287da723a442
Dusko Radicic <radicic.ra129.2019@uns.ac.rs>
2021-11-13 18:15:51 +0100

    New class MainWindow

---
+++ src/gui/MainWindow.java
@@ -0,0 +1,18 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class MainWindow extends JFrame {
+
+	public MainWindow() {
+		super();
+		
+		
+		
+	}
+}
