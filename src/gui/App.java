f1f55c3f843a4ccae52fc2a75d637debf377cba2
Dusko Radicic <radicic.ra129.2019@uns.ac.rs>
2021-11-13 18:51:59 +0100

    Application running changes

--- src/gui/App.java
+++ src/gui/App.java
@@ -6,8 +6,6 @@ public class App {
 
 	public static void main(String[] args) {
 		MainWindow mw = new MainWindow();
-		mw.setVisible(true);
-		mw.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 
 }
