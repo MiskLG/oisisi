177f50ad9a3bae025d865fc63835bda130945ab1
Dusko Radicic <radicic.ra129.2019@uns.ac.rs>
2021-11-13 18:50:43 +0100

    Main window characteristics

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -12,7 +12,22 @@ public class MainWindow extends JFrame {
 	public MainWindow() {
 		super();
 		
+		setVisible(true);
 		
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		
+		setSize(screenWidth / 4 * 3, screenHeight / 4 * 3);
+		
+		setLocation(screenWidth / 8, screenHeight / 8);
+		
+		setTitle("Studenstka služba");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+
 		
 	}
 }
