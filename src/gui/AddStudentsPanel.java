d5a403f8e1578656acaf24df5373e15b302a92f1
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2021-12-04 13:52:16 +0100

    Added design to add student dialog

--- src/gui/AddStudentsPanel.java
+++ src/gui/AddStudentsPanel.java
@@ -1,31 +1,138 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class AddStudentsPanel extends JDialog {
 
 	public AddStudentsPanel(Point location, Dimension size) {
 		this.setModal(true);
+		this.setResizable(false);
 		
 		double widthRatio = 40./100;
 		double heightRatio = 95./100;
 		
-		Double locationX = location.x + size.getWidth() / 2 - size.getWidth() * widthRatio / 2;
-		Double locationY = location.y + size.getHeight() / 2 - size.getHeight() * heightRatio / 2;
-		Double sizeX = size.getWidth() * widthRatio;
-		Double sizeY = size.getHeight() * heightRatio;
-		Double maxX = 1920 * widthRatio;
-		Double maxY = 1080 * heightRatio;
-		Double minX = 960 * widthRatio;
-		Double minY = 540 * heightRatio;
+		Double minX = 1200 * widthRatio;
+		Double minY = 600 * heightRatio;
+	
+		Double locationX = location.x + size.getWidth() / 2 - minX /2 ;
+		Double locationY = location.y + size.getHeight() / 2 - minY / 2;
 		
-		this.setMaximumSize(new Dimension(maxX.intValue(),maxY.intValue()));
-		this.setMinimumSize(new Dimension(minX.intValue(),minY.intValue()));	
+		Double sizeX = minX;
+		Double sizeY = minY;
+		
 		setLocation(locationX.intValue() , locationY.intValue() );
 		setSize(sizeX.intValue(), sizeY.intValue());
 		
+		this.setTitle("Dodavanje studenta");
+
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		
+		JPanel fieldsPanel = new JPanel();
+		JPanel buttonsPanel = new JPanel();
+		
+		fieldsPanel.setLayout(new GridLayout(10,2));
+		
+		JLabel nameLabel = new JLabel("Ime*:");
+		JTextField nameField = new JTextField();
+		fieldsPanel.add(nameLabel);
+		fieldsPanel.add(nameField);
+		
+		JLabel lastnameLabel = new JLabel("Prezime*:");
+		JTextField lastnameField = new JTextField();
+		fieldsPanel.add(lastnameLabel);
+		fieldsPanel.add(lastnameField);
+		
+		JLabel dateobLabel = new JLabel("Datum rođenja*:");
+		JTextField dateobField = new JTextField();
+		fieldsPanel.add(dateobLabel);
+		fieldsPanel.add(dateobField);
+		
+		JLabel adressLabel = new JLabel("Adresa stanovanja*:");
+		JTextField adressField = new JTextField();
+		fieldsPanel.add(adressLabel);
+		fieldsPanel.add(adressField);
+		
+		JLabel phoneLabel = new JLabel("Broj telefona*:");
+		JTextField phoneField = new JTextField();
+		fieldsPanel.add(phoneLabel);
+		fieldsPanel.add(phoneField);
+		
+		JLabel emailLabel = new JLabel("E-mail adresa*:");
+		JTextField emailField = new JTextField();
+		fieldsPanel.add(emailLabel);
+		fieldsPanel.add(emailField);
+		
+		JLabel indexLabel = new JLabel("Broj indeksa*:");
+		JTextField indexField = new JTextField();
+		fieldsPanel.add(indexLabel);
+		fieldsPanel.add(indexField);
+		
+		JLabel enrolmentLabel = new JLabel("Godina upisa*:");
+		JTextField enrolmentField = new JTextField();
+		fieldsPanel.add(enrolmentLabel);
+		fieldsPanel.add(enrolmentField);
+		
+		JLabel yearOfStudyLabel = new JLabel("Trenutna godina studija*:");
+		String  yearChoices[] = {"I (prva)","II (druga)","III (treća)","IV (četvrta)", "V (peta)", "VI (šesta)"};     
+		JComboBox yearOfStudyField = new JComboBox(yearChoices);
+		fieldsPanel.add(yearOfStudyLabel);
+		fieldsPanel.add(yearOfStudyField);
+		
+		JLabel statusLabel = new JLabel("Način finansiranja*:");
+		String  statusChoices[] = {"Budžet", "Samofinansirajuće"};     
+		JComboBox statusField = new JComboBox(statusChoices);
+		fieldsPanel.add(statusLabel);
+		fieldsPanel.add(statusField);
+		
+		
+		mainPanel.setBorder(new EmptyBorder(20,30,20,30));
+		((BorderLayout)mainPanel.getLayout()).setVgap(20);
+		
+		((GridLayout)fieldsPanel.getLayout()).setVgap(15);
+		((GridLayout)fieldsPanel.getLayout()).setHgap(15);
+		
+		indexField.setPreferredSize(new Dimension(15,100));
+		
+		buttonsPanel.setLayout(new FlowLayout());
+		
+		JButton acceptButton = new JButton("Potvrdi");
+		JButton cancelButton = new JButton("Odustani");
+		
+		acceptButton.setPreferredSize(new Dimension(140,30));
+		cancelButton.setPreferredSize(new Dimension(140,30));
+		
+		JPanel spacer = new JPanel();
+		spacer.setPreferredSize(new Dimension(15,30));
+		
+		buttonsPanel.add(acceptButton);
+		buttonsPanel.add(spacer);
+		buttonsPanel.add(cancelButton);
+		
+	
+		cancelButton.addActionListener( e -> { this.dispose(); });
+		
+		
+		
+		mainPanel.add(fieldsPanel, BorderLayout.CENTER);
+		mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+		this.setContentPane(mainPanel);
+
 		this.setVisible(true);
 	}
 	
