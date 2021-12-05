b40b2fb1277594d886922c381f55056a48601cee
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2021-12-05 02:12:03 +0100

    Fixed regx class

--- src/controller/RegXClass.java
+++ src/controller/RegXClass.java
@@ -3,14 +3,16 @@ package controller;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import model.Date;
+
 public class RegXClass {
 	
 	static boolean checkName(String name) {
 		
-		Pattern pattern = Pattern.compile("([A-ZŽĐŠĆČ][a-zžđšćč]*)(([ ]|[-])[A-ZŠĐŽČĆ][a-zšđžčć]*)*",Pattern.UNICODE_CHARACTER_CLASS);	
+		Pattern pattern = Pattern.compile("([A-ZŽĐŠĆČ]{1}[a-zžđšćč]*)(([ ]|[-])[A-ZŠĐŽČĆ]{1}[a-zšđžčć]*)*",Pattern.UNICODE_CHARACTER_CLASS);	
 		Matcher matcher = pattern.matcher(name);
 
-		return matcher.find();
+		return matcher.matches();
 
 	}
 	
@@ -22,11 +24,19 @@ public class RegXClass {
 	
 	static boolean checkDate(String date) {
 		
-		// a big pattern to check kinda valid dates, still needs to go through a check for leap year
-		Pattern pattern = Pattern.compile("((((([3][0-1])|(([0]?[1-9])|([1-2][0-9])))[.](([13578])|([1][0,2])))|((([3][0])|(([0]?[1-9])|([1-2][0-9])))[.](([0]?[468])|([1][1])))) | ((([0]?[1-9])|([1-2][0-9]))[.][0]?[2]))[.](19[3-9][0-9])|20[0-2][0-9]");	
-		Matcher matcher = pattern.matcher(date);
+		// a big pattern to check kinda valid dates, still needs to go through a check for leap year	
 
-		return matcher.find();
+		//Pattern pattern = Pattern.compile("((((([3][0-1])|(([0]?[1-9])|([1-2][0-9])))[.]([13578]|([1][0,2])))|((([3][0])|(([0]?[1-9])|([1-2][0-9])))[.](([0]?[468])|([1][1])))) | ((([0]?[1-9])|([1-2][0-9]))[.][0]?[2]{1}))[.]((19[3-9][0-9])|(20[0-2][0-9]))");	
+		Pattern pattern = Pattern.compile("[0-9]{2}[.][0-9]{2}[.][0-9]{4}");
+		Matcher matcher = pattern.matcher(date);
+		
+		if(true ==  matcher.matches()) {
+			Date d = new Date(date);
+			if(d.getDay() != 0) {
+				return true;
+			}
+		}	
+		return false;
 	}
 	
 	static boolean checkAdress(String adress) {
@@ -34,7 +44,7 @@ public class RegXClass {
 		Pattern pattern = Pattern.compile("(([A-ZŽĐŠĆČ][a-zžđšćč]*)([ ][A-ZŠĐŽČĆ][a-zšđžčć]*)*)[ ]?[,][ ]?[1-9]+[0-9]*[a-z]*[ ]?[,][ ]?(([A-ZŽĐŠĆČ][a-zžđšćč]*)([ ][A-ZŠĐŽČĆ][a-zšđžčć]*)*)[ ]?[,][ ]?(([A-ZŽĐŠĆČ][a-zžđšćč]*)([ ][A-ZŠĐŽČĆ][a-zšđžčć]*)*)",Pattern.UNICODE_CHARACTER_CLASS);	
 		Matcher matcher = pattern.matcher(adress);
 		
-		return true;
+		return matcher.matches();
 	}
 	
 	static boolean checkIndex(String index) {
@@ -42,7 +52,7 @@ public class RegXClass {
 		Pattern pattern = Pattern.compile("([a-zšđžćčA-ZŠĐŽČĆ0-9]{2}-[0-9]{1,3}-19[6-9][0-9])|([a-zA-Z0-9]{2}-[0-9]{1,3}-20[0-2][0-9])",Pattern.UNICODE_CHARACTER_CLASS);	
 		Matcher matcher = pattern.matcher(index);
 
-		return matcher.find();
+		return matcher.matches();
 	}
 	
 	
@@ -51,7 +61,7 @@ public class RegXClass {
 		Pattern pattern = Pattern.compile("(06[0-9]{7,8})|([+][0-9]{11,12})|(00[0-9]{11,12})");		
 		Matcher matcher = pattern.matcher(phone);
 		
-		return matcher.find();
+		return matcher.matches();
 	}
 	
 	static boolean checkEmail(String email) {
@@ -59,7 +69,7 @@ public class RegXClass {
 		Pattern pattern = Pattern.compile("[a-zA-Z0-9,.+]+@([a-zA-Z0-9]+[.])+[a-zA-Z0-9]");
 		Matcher matcher = pattern.matcher(email);
 		
-		return matcher.find();
+		return matcher.matches();
 	}
 	
 	static boolean checkYear(String year) {
@@ -67,6 +77,6 @@ public class RegXClass {
 		Pattern pattern = Pattern.compile("(19[6-9][0-9])|20[0-2][0-9]");	
 		Matcher matcher = pattern.matcher(year);
 		
-		return matcher.find();
+		return matcher.matches();
 	}
 }
