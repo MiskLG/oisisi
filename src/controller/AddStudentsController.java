6aad322295773ff48ad9a9935698b7e299ab0e46
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2021-12-05 02:12:25 +0100

    Fixed add student controller

--- src/controller/AddStudentsController.java
+++ src/controller/AddStudentsController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import main.DataClass;
@@ -25,43 +26,41 @@ public class AddStudentsController {
 
 	public AddStudentsController(String name, String lastname, String date, String adress,  String phone, String email, String index,
 			String year, int yearOfStudy, int status) {
-		this.name = name;
-		this.lastname = lastname;
-		this.date = new Date(date);
-		this.adress = new Adress(adress);
-		this.phone = phone;
-		this.index = index;
-		this.year = Integer.parseInt(year);
-		this.yearOfStudy = yearOfStudy;
-		this.status = status;
-		this.email = email;
+		this.err = checkData(name,lastname,date,adress,phone,email,index,year);
+		if(err.equals("Sve je dobro")) {
+			this.name = name;
+			this.lastname = lastname;
+			this.date = new Date(date);
+			this.adress = new Adress(adress);
+			this.phone = phone;
+			this.index = index;
+			this.year = Integer.parseInt(year);
+			this.yearOfStudy = yearOfStudy;
+			this.status = status;
+			this.email = email;
+		}
 		
-		this.err = checkData(name,lastname,date,adress,phone,index,year);
 	}
 
 
 	public String addStudentToData() {
 		DataClass data = DataClass.getInstance();
 		
-		if(!err.equals("")) {
+		if(!err.equals("Sve je dobro")) {
 			return err;
 		}
 			
-		List<Student> listStudent = data.getStudentListData();
-		for(Student s : listStudent) {
-			if(index.equalsIgnoreCase(s.getIndex())) {
-				return "Index already used!";
-			}
-		}
 		
-		listStudent.add(new Student(this.name, this.lastname, this.date, this.adress, this.phone, this.email, this.index, this.year, this.yearOfStudy, this.status));
+		ArrayList<Student> listStudent = data.getStudentListData();
+		listStudent.add(new Student(this.name, this.lastname, this.date, this.adress, this.phone, this.email, this.index, this.year, this.yearOfStudy+1, this.status));
+		data.setStudentListData(listStudent);
 		
-		return "All good";
+		return err;
 	}
 	
 	
-	public String checkData(String name, String lastname, String date, String adress, String phone, String index, String year) {
-		 err = "All good";
+	public String checkData(String name, String lastname, String date, String adress, String phone, String email, String index, String year) {
+		 err = "Sve je dobro";
 		
 		if(false == RegXClass.checkName(name)) {
 			err = "Loše uneseno ime";
@@ -80,17 +79,33 @@ public class AddStudentsController {
 			return err;
 		}
 		if(false == RegXClass.checkPhone(phone)) {
-			err = "Loše unesen telefon";
+			err = "Loše unesen broj telefona";
+			return err;
+		}
+		if(false == RegXClass.checkEmail(email)) {
+			err = "Loše unesen email";
 			return err;
 		}
 		if(false == RegXClass.checkIndex(index)) {
 			err = "Loše unesen index";
 			return err;
 		}
+		
+		DataClass data = DataClass.getInstance();
+		List<Student> listStudent = data.getStudentListData();
+		if(listStudent != null) {
+			for(Student s : listStudent) {
+				if(index.equalsIgnoreCase(s.getIndex())) {
+					return "Indeks je zauzet!";
+				}
+			}
+		}
+		
 		if(false == RegXClass.checkYear(year)) {
 			err = "Loše unesena godina studija";
 			return err;
 		}
+		
 		
 		
 		return err;
